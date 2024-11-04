66d1f6479855276d25700c889cf33b6cf3df1039
Knowyourdeen <knowyourdeen101@gmail.com>
2024-11-04 10:26:35 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// JavaScript to display a message when the button is clicked
+function displayMessage() {
+    const message = document.getElementById("message");
+    message.textContent = "Hello! You clicked the button!";
+}
